b45a855c0cd90e2033d986588e2d9dbdb9233378
BarkiZed <2118kots@gmail.com>
2025-07-13 01:21:52 +0300

    Create App.jsx

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import WelcomeMessage from './components/WelcomeMessage';
+
+function App() {
+    return (
+        <>
+            <WelcomeMessage />
+            {/* Other existing components can stay here */}
+        </>
+    );
+}
+
+export default App;
