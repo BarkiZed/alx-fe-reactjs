301302c9e5e2b1dbc324d807864138ff238f242b
BarkiZed <2118kots@gmail.com>
2025-07-18 23:24:54 +0300

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,12 +1,18 @@
 // src/App.jsx
 import Counter from './components/Counter';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
   return (
     <div className="App">
       <h1>Counter App</h1>
       <Counter />
     </div>
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
   );
 }
 
