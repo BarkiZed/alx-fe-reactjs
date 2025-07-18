0bd8c8c632e8098a282e9ba52d91ec93c5235bc4
BarkiZed <2118kots@gmail.com>
2025-07-18 22:56:01 +0300

    Create Footer.jsx

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer style={{
+      backgroundColor: '#282c34',
+      color: 'white',
+      textAlign: 'center',
+      padding: '15px 0',
+      marginTop: '20px',
+      position: 'relative',
+      bottom: '0',
+      width: '100%'
+    }}>
+      <p style={{ margin: '0' }}>© 2023 My Travel App. All rights reserved.</p>
+    </footer>
+  );
+}
