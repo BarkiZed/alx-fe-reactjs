f3b2873599faa66e8944c20c1447dcbce38f6f55
BarkiZed <2118kots@gmail.com>
2025-07-18 23:21:55 +0300

    Create App.jsx

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Counter App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
