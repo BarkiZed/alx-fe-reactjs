fe84d91712404c81a9864d246f482ad2453748c4
BarkiZed <2118kots@gmail.com>
2025-07-22 18:28:02 +0300

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import RecipeList from './components/RecipeList'
 import AddRecipeForm from './components/AddRecipeForm'
 import RecipeDetails from './components/RecipeDetails'
+import SearchBar from './components/SearchBar'
+import AdvancedFilters from './components/AdvancedFilters'
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <div className="app">
         <nav>
           <Link to="/">Home</Link>
+          <Link to="/add">Add Recipe</Link>
         </nav>
         
         <h1>Recipe Sharing App</h1>
@@ -17,11 +20,15 @@ function App() {
         <Routes>
           <Route path="/" element={
             <>
-              <AddRecipeForm />
+              <div className="search-section">
+                <SearchBar />
+                <AdvancedFilters />
+              </div>
               <RecipeList />
             </>
           } />
           <Route path="/recipes/:id" element={<RecipeDetails />} />
+          <Route path="/add" element={<AddRecipeForm />} />
         </Routes>
       </div>
     </Router>
