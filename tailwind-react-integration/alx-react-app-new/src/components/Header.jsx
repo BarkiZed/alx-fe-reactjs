3372b9664593b10f52909011031d0b28480459f5
BarkiZed <2118kots@gmail.com>
2025-08-10 16:42:53 +0300

    Properly add tailwind directory

---
+++ tailwind-react-integration/alx-react-app-new/src/components/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <header style={{
+      backgroundColor: '#282c34',
+      color: 'white',
+      textAlign: 'center',
+      padding: '20px 0',
+      boxShadow: '0 2px 5px rgba(0,0,0,0.1)'
+    }}>
+      <h1 style={{
+        margin: '0',
+        fontSize: '2.5rem',
+        letterSpacing: '1px'
+      }}>My Favorite Cities</h1>
+    </header>
+  );
+}
---
+++ tailwind-react-integration/github-user-search/src/App.jsx
@@ -0,0 +1,13 @@
+import Search from './components/Search';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>GitHub User Search</h1>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
