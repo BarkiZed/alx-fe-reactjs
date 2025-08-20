829092c4ca3f0b2279f8b093d463d0a14162f0f5
BarkiZed <2118kots@gmail.com>
2025-08-20 22:53:58 +0300

    Create BlogPost.jsx

---
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+export default function BlogPost() {
+  const { id } = useParams();
+  return (
+    <div>
+      <h1>Blog Post #{id}</h1>
+      <p>This is a dynamically routed blog post.</p>
+    </div>
+  );
+}
